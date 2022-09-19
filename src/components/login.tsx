ea5da94c48a1eaa80678f7588d30d3f619b7a305
SalvadorLekan <lekansalvador@gmail.com>
2022-09-19 18:28:43 +0100

    feat: added login functionality

---
+++ src/components/login.tsx
@@ -0,0 +1,78 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { Formik } from "formik";
+import { useAppDispatch } from "hooks/store";
+import { useCallback } from "react";
+import { login } from "store/slices/user";
+import { object, string } from "yup";
+
+const initialValues = {
+  name: "",
+};
+
+const validationSchema = object({
+  name: string().required("Name is required"),
+});
+
+export default function Login() {
+  const dispatch = useAppDispatch();
+
+  const logIn = useCallback(
+    (user: User) => {
+      dispatch(login(user));
+    },
+    [dispatch]
+  );
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setFieldError }) => {
+          values.name = values.name.toLocaleLowerCase().trim();
+          if (!values.name) {
+            setFieldError("name", "Name is required");
+          } else {
+            logIn(values);
+          }
+        }}>
+        {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+          <Box
+            sx={{
+              width: "100%",
+              mx: "auto",
+              maxWidth: "400px",
+            }}
+            component="form"
+            onSubmit={handleSubmit}>
+            <Typography variant="h5" component="h1">
+              Login
+            </Typography>
+            <TextField
+              size="small"
+              label="Username"
+              name="name"
+              margin="normal"
+              fullWidth
+              helperText={touched.name && errors.name}
+              error={touched.name && Boolean(errors.name)}
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <Button type="submit" variant="contained" size="large" fullWidth>
+              Login
+            </Button>
+          </Box>
+        )}
+      </Formik>
+    </Container>
+  );
+}
